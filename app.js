0fd8f4eb2fb617eb454d16d3690f8a3b248ec3b0
Krekus <58678923+iKrekus@users.noreply.github.com>
2024-03-12 17:49:51 +0300

    1

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-const taskTitle = document.querySelector('#task-title');
-const taskDescription = document.querySelector('#task-description');
+const taskTitleField = document.querySelector('#task-title');
+const taskDescriptionField = document.querySelector('#task-description');
 const buttonAdd = document.querySelector('#btn-add');
-const tasksList = document.querySelector('.task-mgr__list');
+const taskTitle = document.querySelector('.task-mgr__list');
+const taskTemplate = document.querySelector('#task-tmpl');
 
 buttonAdd.addEventListener('click', function () {
-  if (taskTitle.value && taskDescription.value) {
-  addTask();
+  if (taskTitleField.value && taskDescriptionField.value) {
+  // addTask();
+  createTask(taskTitleField.value, taskDescriptionField.value);
   clear();
+  console.log(createTask);
   }
 
 })
 
 function clear(){
-  taskTitle.value='';
-  taskDescription.value='';
+  taskTitleField.value='';
+  taskDescriptionField.value='';
 };
 
-function addTask (element) {
-  const task = document.createElement('li')
-  task.innerHTML = `
-    <span>${taskTitle.value}<span>
-    <span>${taskDescription.value}<span>`;
-  tasksList.appendChild(task)
+
+function createTask (name, description) {
+  // console.log(taskTemplate);
+  const task = taskTemplate.cloneNode(true);
+  console.log(task);
+  let aa = taskElement.querySelector('.task-title');
+  console.log(aa);
+  // const taskTitle = task.querySelector('.task-title');
+  // const taskDescription = task.querySelector('.task-description');
+
+
+  // taskTitle.value = name;
+  // taskDescription.value = description;
+
+  // return task;
 }
+
+function renderTask () {
+
+}
+
+// function addTask (element) {
+//   const task = document.createElement('li')
+//   task.innerHTML = `
+//     <span>${taskTitleField.value}<span>
+//     <span>${taskDescriptionField.value}<span>`;
+//   tasksList.appendChild(task)
+// }
